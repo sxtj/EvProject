8cdc1a71e8d0b4432c318e334ec9b8e2df9635fb
Ethan Higgins <syphaeric@tutanota.com>
2024-01-02 17:30:17 -0700

    initial commit

---
+++ evproject/src/main/java/com/ethanscompany/evproject/controller/EvProjectController.java
@@ -0,0 +1,16 @@
+package com.ethanscompany.evproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EvProjectController {
+	
+	//@GetMapping("/test")
+	@PostMapping("test")
+	public String test() {
+		return "Hello world!";
+	}
+
+}
---
+++ evproject/src/test/java/com/ethanscompany/evproject/EvprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethanscompany.evproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
