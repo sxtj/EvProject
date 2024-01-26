89abfe59d5b65723224042c0c6bc9c13ca6f465f
Jarek <jarek.krych@softwarexpressllc.com>
2024-01-25 17:09:22 -0700

    Merge branch 'develop' of git@github.com:sxtj/EvProject.git into develop Added new code to EV controller class.

--- evproject/src/main/java/com/ethanscompany/evproject/controller/EvProjectController.java
+++ evproject/src/main/java/com/ethanscompany/evproject/controller/EvProjectController.java
@@ -2,7 +2,10 @@ package com.ethanscompany.evproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ethanscompany.evproject.model.EvChargingStation;
 
 @RestController
 public class EvProjectController {
@@ -10,6 +13,14 @@ public class EvProjectController {
 	@GetMapping("/test")
 	public String test() {
 		return "Hello world!";
+	}
+	
+	@PostMapping("/evChargingStations")
+	public boolean addEvChargingStation(@RequestBody(required=false) EvChargingStation evChargingStation) {
+		if(evChargingStation != null) {
+			return true;
+		}
+		return false;
 	}
 
 }
