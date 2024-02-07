8d9881fba08d0b68bee484b171871a943f2618b1
Jarek <jarek.krych@softwarexpressllc.com>
2024-02-06 17:05:58 -0700

    Added new Cascade EV charging station.

--- evproject/src/main/java/com/ethanscompany/evproject/service/EvChargingStationService.java
+++ evproject/src/main/java/com/ethanscompany/evproject/service/EvChargingStationService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.ethanscompany.evproject.model.EvChargingStation;
+import com.ethanscompany.evproject.model.Level;
 import com.ethanscompany.evproject.model.Location;
 
 @Service
@@ -26,8 +27,22 @@ public class EvChargingStationService {
 		EvChargingStation manitouEvStation = new EvChargingStation();
 		manitouEvStation.setId(124);
 		manitouEvStation.setLocation(location);
+		manitouEvStation.setLevel(Level.LEVEL_2);
+		manitouEvStation.setStatus(true);
+		manitouEvStation.setPricePerKWH(0.39);
+		
+		EvChargingStation cascadeEvStation = new EvChargingStation();
+		cascadeEvStation.setId(1245);
+		Location location2 = new Location();
+		location2.setCity("Cascade");
+		location2.setState("CO");
+		cascadeEvStation.setLocation(location2);
+		cascadeEvStation.setLevel(Level.LEVEL_2);
+		cascadeEvStation.setStatus(true);
+		cascadeEvStation.setPricePerKWH(0.39);
 		
 		allEvChargingStations.add(manitouEvStation);
+		allEvChargingStations.add(cascadeEvStation);
 		
 		return allEvChargingStations;
 		
